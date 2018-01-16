81f5d15b714c795bfe391fd57d58b48abbb208d9
Shai James <skyandbeyond@gmail.com>
2018-01-16 09:14:14 +0200

    identities

--- src/models/Keychain.ts
+++ src/models/Keychain.ts
@@ -1,27 +1,30 @@
 import Wallet from "./Wallet";
+import {Identity} from "./Identity";
 
 export class Keychain {
 	wallets:Array<Wallet>;
+	identities:Array<Identity>;
 	locked:boolean;
 
 	constructor(){
         this.wallets = null;
+        this.identities = null;
         this.locked = null;
 	}
 
 	static placeholder() {
 		let p = new Keychain();
 		p.wallets = [];
+		p.identities = [];
         p.locked = true;
 		return p;
 	}
 
 	static fromJson(jsonOrEncryptedString) {
-		if(typeof jsonOrEncryptedString === 'object') {
-            let p = Object.assign(this.placeholder(), jsonOrEncryptedString);
-            p.wallets = jsonOrEncryptedString.wallets.map(x => Wallet.fromJson(x));
-            return p;
-        } else return jsonOrEncryptedString;
+		let p = Object.assign(this.placeholder(), jsonOrEncryptedString);
+		p.wallets = jsonOrEncryptedString.wallets.map(x => Wallet.fromJson(x));
+		p.identities = jsonOrEncryptedString.identities.map(x => Identity.fromJson(x));
+		return p;
 	}
 
 	static hasUnreclaimedKey(wallets){ return wallets.filter(x => x.find(z => z.reclaimed)).length }
